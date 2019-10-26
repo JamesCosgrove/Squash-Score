e51f6c5557e1e5923f5d465f7986ea03ab48fed6
James Cosgrove <jamescosgrove2000@live.co.uk>
2019-10-26 19:14:11 +0100

    Initial Commit

---
+++ Squash Score WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Squash Score WatchKit Extension
+//
+//  Created by James Cosgrove on 26/10/2019.
+//  Copyright © 2019 James Cosgrove. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ Squash Score WatchKit Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  Squash Score WatchKit Extension
+//
+//  Created by James Cosgrove on 26/10/2019.
+//  Copyright © 2019 James Cosgrove. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
